d82a75d27455ea89059620980144d76e77c7069e
Mhonns <nsamaira@cmkl.ac.th>
2025-01-24 19:48:28 +0700

    Add init requirement

--- init/install-chv.sh
+++ init/install-chv.sh
@@ -1,6 +1,9 @@
 wget https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/v43.0/cloud-hypervisor
 sudo mv cloud-hypervisor /bin/cloud-hypervisor
+chmod +x /usr/bin/cloud-hypervisor 
 
-mkdir ../vms-config/storage
+mkdir ../vms-config
+mkdir ../os
+mkdir ../storage
 
-wget https://github.com/cloud-hypervisor/rust-hypervisor-firmware/releases/download/0.4.2/hypervisor-fw
+wget https://github.com/cloud-hypervisor/rust-hypervisor-firmware/releases/download/0.4.2/hypervisor-fw -P ../os/
