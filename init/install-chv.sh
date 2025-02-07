b9a771cc29718c07d862ee0738c7de4779fb4956
Mhonns <nsamaira@cmkl.ac.th>
2025-02-07 21:33:23 +0700

    Also install ch-remote bin

--- init/install-chv.sh
+++ init/install-chv.sh
@@ -1,6 +1,9 @@
-wget https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/v43.0/cloud-hypervisor
+wget https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/v44.0/cloud-hypervisor
+wget https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/v44.0/ch-remote
 sudo mv cloud-hypervisor /bin/cloud-hypervisor
+sudo mv ch-remote /bin/ch-remote
 chmod +x /usr/bin/cloud-hypervisor 
+chmod +x /usr/bin/ch-remote
 
 mkdir ../vms-config
 mkdir ../os
